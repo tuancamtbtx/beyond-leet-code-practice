92227dc7fdb440ae6b03b2475160d3593847d032
tuan.nguyen15 <tuan.nguyen15@tiki.vn>
2022-07-30 22:56:38 +0700

    [add] search in tree

--- data_structure/trees/binary_search_tree_v1.py
+++ data_structure/trees/binary_search_tree_v1.py
@@ -17,20 +17,41 @@ class Node():
                 self.right = Node(value)
             else:
                 self.right.insert(value)
+
     def print_tree(self):
         if self.left:
             self.left.print_tree()
+        print(self.value)
         if self.right:
             self.right.print_tree()
-        print(self.value)
 
+    def find_x(self, value):
+        print(value, self.value)
+        if value < self.value:
+            print("<")
+            if self.left is None:
+                return False
+            return self.left.find_x(value)
+        elif value > self.value:
+            print(">")
+            if self.right is None:
+                return False
+            return self.right.find_x(value)
+        else:
+            print("==")
+            return True # found value
 def main():
+
     root = Node(12)
+    root.insert(-1)
+    root.insert(1)
     root.insert(1)
     root.insert(2)
     root.insert(20)
     root.insert(100)
-    root.print_tree()
+    # root.print_tree()
+    x = root.find_x(1100)
+    print(x)
 
 if __name__ == "__main__":
     main()
