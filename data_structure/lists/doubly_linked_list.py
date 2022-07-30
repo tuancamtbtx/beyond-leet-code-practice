03e21a33b4209fd008766681418694a53fb4675b
tuan.nguyen15 <tuan.nguyen15@tiki.vn>
2022-07-31 00:31:31 +0700

    [add] full linked list

---
+++ data_structure/lists/doubly_linked_list.py
@@ -0,0 +1,4 @@
+class Node():
+	def __init__(self, value):
+		self.next = None
+		self.value = value
--- data_structure/lists/linked_list.py
+++
@@ -1,5 +0,0 @@
-class Node():
-	def __init__(self, data):
-		self.data = data
-		self.prev = None
-		self.next = None
