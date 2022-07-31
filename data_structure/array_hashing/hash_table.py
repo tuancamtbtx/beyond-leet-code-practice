d66a890d4e578bd43f5e8b7f94b5b5523716b471
tuan.nguyen15 <tuan.nguyen15@tiki.vn>
2022-07-31 22:45:07 +0700

    [add] hash table

--- data_structure/array_hashing/hash_table.py
+++ data_structure/array_hashing/hash_table.py
@@ -1,3 +1,35 @@
 class HashTable():
-	def __init__(self,size):
-		self.size = size
+    def __init__(self,size):
+        self.size = size
+        self.hash_table = self.create_buckets()
+    
+    def create_buckets(self):
+        return [ [] for _ in range(self.size)]
+    
+    def set_value(self, key, value):
+        hashed_key =  hash(key) % self.size
+
+        bucket = self.hash_table[hashed_key]
+        found_key = False
+
+        for index, record in enumerate(bucket):
+            record_key , record_value = record
+            if record_key == key:
+                found_key = True
+                break
+        if found_key == True:
+            bucket[index] = (key, value)
+        else:
+            bucket.append((key, value))
+    def __str__(self):
+        return "".join(str(item) for item in self.hash_table)
+
+def main():
+    hash_table = HashTable(10)
+    hash_table.set_value("a", 1)
+    hash_table.set_value("a", 2)
+    hash_table.set_value("tua", "quanma")
+
+    print(hash_table)
+if __name__ == "__main__":
+    main()
